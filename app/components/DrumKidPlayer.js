d08620223434e875002db4ef6183e48cefcd7f98
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-08-03 16:33:55 +0100

    testing with a simple tone instead of drum sounds for now

--- app/components/DrumKidPlayer.js
+++ app/components/DrumKidPlayer.js
@@ -3,21 +3,39 @@ import React from 'react';
 export default class DrumKidPlayer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+    }
   }
   componentDidMount() {
-
+     var audioContext = new (AudioContext||webkitAudioContext)();
+     var outputNode = audioContext.createGain();
+     outputNode.connect(audioContext.destination);
+     outputNode.gain.value = 0.1;
+     this.setState({
+       audioContext,
+       outputNode
+     })
   }
   playBeat() {
-    alert("PLAY");
+    // temp thing to demonstrate web audio working
+    var oscillator = this.state.audioContext.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.value = 440;
+    oscillator.connect(this.state.outputNode);
+    oscillator.start();
+    this.setState({
+      oscillator
+    })
   }
   stopBeat() {
-    alert("STOP");
+    if(this.state.oscillator) this.state.oscillator.stop();
   }
   render() {
     return (
       <div>
-        <a onClick={this.playBeat}>Play</a><span> </span>
-        <a onClick={this.stopBeat}>Stop</a><span> </span>
+        <a onClick={this.playBeat.bind(this)}>Play</a><span> </span>
+        <a onClick={this.stopBeat.bind(this)}>Stop</a><span> </span>
       </div>
     )
   }
